22b799563d6a1a6de5c880145afabb110c86d76a
Eduardo Spiegel21 <eduardospiegel@MacBook-Pro-de-Eduardo.local>
2024-12-04 22:11:51 -0800

    32 layers is too much

--- src/main.py
+++ src/main.py
@@ -34,7 +34,7 @@ def main():
         "ENZYMES": load_dataset(root='/tmp/ENZYMES', name='ENZYMES', device=device)
     }
 
-    layer_configs = [2, 4, 8, 16, 32]
+    layer_configs = [2, 4, 8, 16]
     model_classes = {
         "Cora": {"GCN": GCNModel, "GAT": GATModel, "GIN": GINModel},
         "IMDB-BINARY": {"GCN": GCNGraphClassifier, "GAT": GATGraphClassifier, "GIN": GINGraphClassifier},
