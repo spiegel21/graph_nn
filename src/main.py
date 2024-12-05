c99af26bf9ee404ee91754c3d3bf4b31c5bc4923
Eduardo Spiegel21 <eduardospiegel@MacBook-Pro-de-Eduardo.local>
2024-12-04 21:42:42 -0800

    changed hyperparameters

--- src/main.py
+++ src/main.py
@@ -34,7 +34,7 @@ def main():
         "ENZYMES": load_dataset(root='/tmp/ENZYMES', name='ENZYMES', device=device)
     }
 
-    layer_configs = [2, 3, 4, 5, 6, 7, 8]
+    layer_configs = [2, 4, 8, 16, 32]
     model_classes = {
         "Cora": {"GCN": GCNModel, "GAT": GATModel, "GIN": GINModel},
         "IMDB-BINARY": {"GCN": GCNGraphClassifier, "GAT": GATGraphClassifier, "GIN": GINGraphClassifier},
